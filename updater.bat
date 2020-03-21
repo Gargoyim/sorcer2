569a786d211284d913ad0ef4beb0090c2ef67971
Gargoyim <MVail95@Yahoo.com>
2020-03-21 05:53:32 -0500

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -1,9 +1,11 @@
 ECHO OFF
-ECHO Downloading OpenComputers...
-curl -L -o OpenComputers-MC1.7.10-1.7.5.1290-universal.jar "https://drive.google.com/uc?export=download&id=1KKxN71Qutja_ArIcm8YRvj7QY7uu-6hL"
-ECHO Moving to mods folder
-move OpenComputers-MC1.7.10-1.7.5.1290-universal.jar mods
+ECHO Checking for all changes made through these updates so far...
+IF EXIST "mods/OpenComputers-MC1.7.10-1.7.5.1290-universal.jar" (
+    ECHO OpenComputers is already installed!
+) ELSE (
+    ECHO OpenComputers is not installed! Downloading...
+    curl -L -o OpenComputers-MC1.7.10-1.7.5.1290-universal.jar "https://drive.google.com/uc?export=download&id=1KKxN71Qutja_ArIcm8YRvj7QY7uu-6hL"
+    ECHO Moving to mods folder
+    move OpenComputers-MC1.7.10-1.7.5.1290-universal.jar mods
+)
 ECHO Update complete!
-ECHO Changes:
-ECHO -Added OpenComputers, a mod I thought I added at the start. Whoops!
-PAUSE
