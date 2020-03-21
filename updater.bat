21a63b5df364951ebc7b8666157f5d050dffbc78
Gargoyim <MVail95@Yahoo.com>
2020-03-21 05:23:46 -0500

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -1,3 +1,9 @@
-ECHO OFF
-ECHO Hello World
+#!/bin/bash
+ECHO Downloading OpenComputers...
+curl -L -o OpenComputers-MC1.7.10-1.7.5.1290-universal.jar "https://drive.google.com/uc?export=download&id=1KKxN71Qutja_ArIcm8YRvj7QY7uu-6hL"
+ECHO Moving to mods folder
+move OpenComputers-MC1.7.10-1.7.5.1290-universal.jar mods
+ECHO Update complete!
+ECHO Changes:
+ECHO -Added OpenComputers, a mod I thought I added at the start. Whoops!
 PAUSE
