c069b2065261a00a06c2cf87bebb674cd2f411d4
Gargoyim <MVail95@Yahoo.com>
2020-03-22 17:34:17 -0500

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -1,5 +1,6 @@
 ECHO OFF
 ECHO Updating the updater-downloading script. My continued life is meaningless!
 powershell -noprofile -command "(New-Object Net.WebClient).DownloadFile(\"https://raw.githubusercontent.com/Gargoyim/sorcer2/master/sorcerupdater.bat\", 'sorcerupdater.bat')"
+call sorcerupdater.bat
 ECHO Committing ritualistic seppuku. Goodbye, cruel world!
 del updater.bat
