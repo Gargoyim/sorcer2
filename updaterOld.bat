0744875b2369c4d9f3ad78cc661c46297f484b4b
Gargoyim <MVail95@Yahoo.com>
2020-03-22 18:12:31 -0500

    Create updaterOld.bat

---
+++ updaterOld.bat
@@ -0,0 +1,15 @@
+ECHO OFF
+ECHO Checking for all changes made through these updates so far...
+IF EXIST "mods/OpenComputers-MC1.7.10-1.7.5.1290-universal.jar" (
+    ECHO OpenComputers is already installed!
+) ELSE (
+    ECHO OpenComputers is not installed! Downloading...
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=1KKxN71Qutja_ArIcm8YRvj7QY7uu-6hL', 'OpenComputers-MC1.7.10-1.7.5.1290-universal.jar')"
+    ECHO Moving to mods folder
+    move OpenComputers-MC1.7.10-1.7.5.1290-universal.jar mods
+)
+IF EXIST "mods/SpiceOfLife-mc1.7.10-1.3.11.bat" (
+    ECHO Spice of Life shouldn't be there!
+    del "mods/SpiceOfLife-mc1.7.10-1.3.11.bat"
+    )
+ECHO Update complete! 
