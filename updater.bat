134f56dc345cc50a370624f253e00a7f81aab1ee
Gargoyim <MVail95@Yahoo.com>
2020-03-22 17:30:41 -0500

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -1,11 +1,5 @@
 ECHO OFF
-ECHO Checking for all changes made through these updates so far...
-IF EXIST "mods/OpenComputers-MC1.7.10-1.7.5.1290-universal.jar" (
-    ECHO OpenComputers is already installed!
-) ELSE (
-    ECHO OpenComputers is not installed! Downloading...
-    curl -L -o OpenComputers-MC1.7.10-1.7.5.1290-universal.jar "https://drive.google.com/uc?export=download&id=1KKxN71Qutja_ArIcm8YRvj7QY7uu-6hL"
-    ECHO Moving to mods folder
-    move OpenComputers-MC1.7.10-1.7.5.1290-universal.jar mods
-)
-ECHO Update complete!
+ECHO Updating the updater-downloading script. My continued life is meaningless!
+powershell -noprofile -command "(New-Object Net.WebClient).DownloadFile(\"https://raw.githubusercontent.com/Gargoyim/sorcer2/master/sorcerupdater.bat\", 'sorcerupdater.bat')"
+ECHO Committing ritualistic seppuku. Goodbye, cruel world!
+del updater.bat
