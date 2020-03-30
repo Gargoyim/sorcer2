d2debdadf601c94867319dddcabb1e0a4888fec5
Gargoyim <MVail95@Yahoo.com>
2020-03-29 20:13:21 -0500

    Update updaterOld.bat

--- updaterOld.bat
+++ updaterOld.bat
@@ -18,7 +18,6 @@ IF EXIST "mods/AdvancedRocketry-1.7.10-1.0.19b.jar" (
     move AdvancedRocketry-1.7.10-1.0.19b.jar mods
 )
 
-LibVulpes-1.7.10-0.1.19-universal.jar
 IF EXIST "mods/LibVulpes-1.7.10-0.1.19-universal.jar" (
     ECHO LibVulpes is already installed!
 ) ELSE (
@@ -26,6 +25,42 @@ IF EXIST "mods/LibVulpes-1.7.10-0.1.19-universal.jar" (
     powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=1UPtO7hE2VB50hDKY85zlJiA2cIzObp_8', 'LibVulpes-1.7.10-0.1.19-universal.jar')"
     ECHO Moving to mods folder
     move LibVulpes-1.7.10-0.1.19-universal.jar mods
+)
+
+IF EXIST "mods/Computronics-1.7.10-1.6.6.jar" (
+    ECHO Computronics is already installed!
+) ELSE (
+    ECHO Computronics is not installed! Downloading...
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=1Bu4S-4hATgaasIUvYJ9eRbWFkG2Fo_QX', 'Computronics-1.7.10-1.6.6.jar')"
+    ECHO Moving to mods folder
+    move Computronics-1.7.10-1.6.6.jar mods
+)
+
+IF EXIST "mods/OpenSecurity-1.7.10-1.0-117.jar" (
+    ECHO OpenSecurity is already installed!
+) ELSE (
+    ECHO OpenSecurity is not installed! Downloading...
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=19c0BnFsWwXbO7kOez1DC0iggtw6iT4Zd', 'OpenSecurity-1.7.10-1.0-117.jar')"
+    ECHO Moving to mods folder
+    move OpenSecurity-1.7.10-1.0-117.jar mods
+)
+
+IF EXIST "mods/AsieLib-1.7.10-0.4.9.jar" (
+    ECHO AsieLib is already installed!
+) ELSE (
+    ECHO AsieLib is not installed! Downloading...
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=1KyjvTGnZuYYXoQXD8qN2nMoTh84kWOaZ', 'AsieLib-1.7.10-0.4.9.jar')"
+    ECHO Moving to mods folder
+    move AsieLib-1.7.10-0.4.9.jar mods
+)
+
+IF EXIST "mods/Animals-Plus-Mod-1.7.10.jar" (
+    ECHO Animals Plus is already installed!
+) ELSE (
+    ECHO Animals Plus is not installed! Downloading...
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=1KQb4eTSkWp8OThv9R7e51NFHyRR6jX0I', 'Animals-Plus-Mod-1.7.10.jar')"
+    ECHO Moving to mods folder
+    move Animals-Plus-Mod-1.7.10.jar mods
 )
 
 IF EXIST "mods/SpiceOfLife-mc1.7.10-1.3.11.jar" (
